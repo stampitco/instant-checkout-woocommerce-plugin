d48b1526459783db831b6ab55f5e526e85aadbea
Vladimir Runchev <vlatkorun@gmail.com>
2021-03-12 09:58:19 +0100

    timeout lowered to 5sec on order done

--- assets/public/checkout/js/Checkout.js
+++ assets/public/checkout/js/Checkout.js
@@ -55,7 +55,7 @@ Checkout.prototype.onCheckoutOrderPlaced = async function onCheckoutOrderPlaced(
         if( this.options.isCartPage() ) {
             window.location.reload();
         }
-    }, 10000 );
+    }, 5000 );
 };
 
 Checkout.prototype.onCheckoutWindowClosed = function onCheckoutWindowClosed() {
